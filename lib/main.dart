365eda6cf5de27ec0a9a45b329b6f635e5ecdbfb
Summer <samar.khaleed.309@Gmail.com>
2023-04-11 00:43:15 +0200

    conflicts solved

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:full_circle/Screens/getStarted-page.dart';
 import 'package:full_circle/Screens/home-page.dart';
 import 'package:full_circle/Screens/login-page.dart';
-<<<<<<< HEAD
 import 'package:full_circle/Screens/onboarding-page.dart';
-=======
-import 'package:full_circle/design.dart';
 import 'Screens/donationForm.dart';
->>>>>>> donationForm
 import 'Screens/signup-page.dart';
 import 'Screens/welcome-page.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -27,22 +23,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       initialRoute: GetStarted.id,
-=======
-      initialRoute: DonationForm.id,
->>>>>>> donationForm
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
         RegisterScreen.id: (context) => RegisterScreen(),
         HomeScreen.id: (context) => HomeScreen(),
         LoginScreen.id: (context) => LoginScreen(),
-<<<<<<< HEAD
         OnBoardingScreen.id: (context) => OnBoardingScreen(),
         GetStarted.id: (context) => GetStarted(),
-=======
         DonationForm.id: (context) => DonationForm(),
->>>>>>> donationForm
       },
     );
   }
