6be02aa791f788af4a72903755fc3a0447307f59
Summer <samar.khaleed.309@Gmail.com>
2023-05-09 19:52:19 +0300

    merging instructions

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: GetStarted.id,
+     // initialRoute: GetStarted.id,
       //initialRoute: Recipientsinstructions.id,
-      //initialRoute: MyScreen.id,
+      initialRoute: MyScreen.id,
       routes: {
         WelcomeScreen.id: (context) => const WelcomeScreen(),
         RegisterScreen.id: (context) => const RegisterScreen(),
-        //id bta3 el page bta3k example: instructions.id
         MyScreen.id: (context) => const MyScreen(),
         Recipientsinstructions.id: (context) => const Recipientsinstructions(),
         HomeScreen.id: (context) => const HomeScreen(),
